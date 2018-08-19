5f98ea038a6f7c4105e9884a25864df2bcb274e3
Douglas de Jesus Fonseca <douglas.vasco79@gmail.com>
2018-08-19 20:27:49 -0300

    Refatorando Prototype

--- src/br/com/padroes/criacional/prototypemanager/Main.java
+++ src/br/com/padroes/criacional/prototypemanager/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		
 		Circulo c = new Circulo();
 		Triangulo t = new Triangulo();
 		Square s = new Square();
 		
-		IPrototype prototypo;
+		IPrototype prototypo= null;
 		
 		prototypo= c.clone();
 		prototypo = t.clone();
